9fa1efdeb4111779e2b944a2a67183448c5b3d0d
uhm0311 <uhm0311@naver.com>
2020-03-27 17:17:49 +0900

    Remove custom exception. Rename EventListener to Listener.

--- EmitterSharp/Emitter.cs
+++ EmitterSharp/Emitter.cs
@@ -7,49 +7,42 @@ using System.Runtime.InteropServices;
 namespace EventEmitterSharp
 {
     /// <summary>
-    /// C# implementation of <see href="https://github.com/component/emitter">Emitter in JavaScript module</see>.
+    /// C# implementation of <see href="https://github.com/component/emitter">Emitter</see> in JavaScript module.
     /// </summary>
     public abstract class Emitter<E, T>
     {
-        private readonly ConcurrentDictionary<E, List<Listener<T>>> EventListeners = new ConcurrentDictionary<E, List<Listener<T>>>();
+        private readonly ConcurrentDictionary<E, List<Listener<T>>> Listeners = new ConcurrentDictionary<E, List<Listener<T>>>();
         private readonly object EventMutex = new object();
 
         public Emitter<E, T> On(E Event, Action<T> Callback)
         {
-            return AddEventListener(Event, Callback, false);
+            return AddListener(Event, Callback, false);
         }
 
         public Emitter<E, T> On(E Event, Action Callback)
         {
-            return AddEventListener(Event, Callback, false);
+            return AddListener(Event, Callback, false);
         }
 
         public Emitter<E, T> Once(E Event, Action<T> Callback)
         {
-            return AddEventListener(Event, Callback, true);
+            return AddListener(Event, Callback, true);
         }
 
         public Emitter<E, T> Once(E Event, Action Callback)
         {
-            return AddEventListener(Event, Callback, true);
+            return AddListener(Event, Callback, true);
         }
 
-        private Emitter<E, T> AddEventListener(E Event, Delegate Callback, bool Once)
+        private Emitter<E, T> AddListener(E Event, Delegate Callback, bool Once)
         {
-            if (Event != null && Callback != null)
+            bool IsGenericAction = Callback is Action<T>;
+
+            if (Event != null && (IsGenericAction || Callback is Action))
             {
-                Listener<T> Listener;
+                Listener<T> Listener = IsGenericAction ? new Listener<T>(Callback as Action<T>, Once) : new Listener<T>(Callback as Action, Once);
 
-                if (Callback is Action)
-                {
-                    Listener = new Listener<T>(Callback as Action, Once);
-                }
-                else
-                {
-                    Listener = new Listener<T>(Callback as Action<T>, Once);
-                }
-
-                EventListeners.AddOrUpdate(Event, (_) => new List<Listener<T>>() { Listener }, (_, Listeners) =>
+                Listeners.AddOrUpdate(Event, (_) => new List<Listener<T>>() { Listener }, (_, Listeners) =>
                 {
                     SimpleMutex.Lock(EventMutex, () => Listeners.Add(Listener));
 
@@ -62,7 +55,7 @@ namespace EventEmitterSharp
 
         public Emitter<E, T> Off()
         {
-            EventListeners.Clear();
+            Listeners.Clear();
 
             return this;
         }
@@ -83,11 +76,11 @@ namespace EventEmitterSharp
             {
                 if (Callback == null)
                 {
-                    EventListeners.TryRemove(Event, out _);
+                    Listeners.TryRemove(Event, out _);
                 }
                 else
                 {
-                    if (EventListeners.TryGetValue(Event, out List<Listener<T>> Listeners))
+                    if (this.Listeners.TryGetValue(Event, out List<Listener<T>> Listeners))
                     {
                         SimpleMutex.Lock(EventMutex, () =>
                         {
@@ -125,20 +118,20 @@ namespace EventEmitterSharp
         {
             if (Event != null)
             {
-                if (EventListeners.TryGetValue(Event, out List<Listener<T>> Listeners))
+                if (this.Listeners.TryGetValue(Event, out List<Listener<T>> Listeners))
                 {
                     for (int i = Listeners.Count - 1; i >= 0; i--)
                     {
                         SimpleMutex.Lock(EventMutex, () =>
                         {
-                            Listener<T> EventListener = Listeners[i];
+                            Listener<T> Listener = Listeners[i];
 
-                            if (EventListener.Once)
+                            if (Listener.Once)
                             {
                                 Listeners.RemoveAt(i);
                             }
 
-                            EventListener.Invoke(Argument);
+                            Listener.Invoke(Argument);
                         });
                     }
                 }
@@ -147,24 +140,37 @@ namespace EventEmitterSharp
             return this;
         }
 
-        public List<Delegate> GetListeners(E Event)
+        public List<Delegate> GetListenerList(E Event)
         {
             List<Delegate> Result = new List<Delegate>();
 
-            if (Event != null && EventListeners.TryGetValue(Event, out List<Listener<T>> Listeners))
+            if (Event != null && this.Listeners.TryGetValue(Event, out List<Listener<T>> Listeners))
             {
-                foreach (Listener<T> Listener in Listeners)
+                SimpleMutex.Lock(EventMutex, () =>
                 {
-                    Result.Add(Listener.Callback);
-                }
+                    foreach (Listener<T> Listener in Listeners)
+                    {
+                        Result.Add(Listener.Callback);
+                    }
+                });
             }
 
             return Result;
         }
 
-        public bool HasListeners(E Event)
+        public int GetListenerCount(E Event)
         {
-            return Event != null && EventListeners.TryGetValue(Event, out List<Listener<T>> Listeners) && Listeners.Count > 0;
+            if (this.Listeners.TryGetValue(Event, out List<Listener<T>> Listeners))
+            {
+                return Listeners.Count;
+            }
+
+            return 0;
+        }
+
+        public bool HasListener(E Event)
+        {
+            return Event != null && GetListenerCount(Event) > 0;
         }
     }
 }
